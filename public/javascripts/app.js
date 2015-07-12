08abf1f84cce343a9dabb15eceb6cfbbed3d476c
kid84 <hashikawa.takuya@gmail.com>
2015-07-12 16:45:30 +0900

    modal

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,4 +7,10 @@ $(function(){
   var headerHeight = $(".header").innerHeight();
   console.log(headerHeight);
   $(".contents").css({top:headerHeight});
+
+  $(".js-modal-trigger").on('click', function(){
+    console.log("test");
+    $(".js-modal-contents").show();
+  });
+
 });
